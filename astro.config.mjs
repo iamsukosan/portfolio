fc9faebabc2b6c8919c4f7cdbf320f074252444b
iamsukosan <mr.sud23@gmail.com>
2026-03-12 23:26:09 +0530

    cloudflare adapter configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://portfolio.iamsukosan.com',
-  output: 'static',
+
+  output: 'server',
+
+  adapter: cloudflare(),
+
   integrations: [
     mdx(),
     react(),
     sitemap(),
   ],
+
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
